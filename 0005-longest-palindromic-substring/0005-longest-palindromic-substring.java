df7a728464fb90f23be5e800a1e76124dfbcc8bb
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2024-10-14 08:15:43 +0530

    Time: 19 ms (65.2%), Space: 45.3 MB (28.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,11 +1,31 @@
 class Solution {
 
+    public String solveDp(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int len = 0;
+        String ans = "";
+        for(int i = 0; i<n; i++) {
+            dp[i][i] = true;
+            for(int j = 0; j<=i; j++) {
+                if(s.charAt(i)==s.charAt(j) && (i-j<=2 || dp[i-1][j+1] == true)) {
+                    dp[i][j] = true;
+                    if(i-j+1 > len) {
+                        len = i-j+1;
+                        ans = s.substring(j, i+1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
     public String longestPalindrome(String s) {
         int n = s.length();
         int len = 0;
         String ans = "";
         if(n == 1) return s;
-
+        // return solveDp(s);
         for(int i = 0; i<n; i++) {
             int l = i, r = i;
 
