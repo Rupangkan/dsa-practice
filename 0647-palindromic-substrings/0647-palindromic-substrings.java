94e895526138de499fc5daf1c5fa870c3e128321
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2024-10-14 17:58:20 +0530

    Time: 6 ms (75.36%), Space: 41 MB (96.73%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,8 +1,26 @@
 class Solution {
+    int solveDp(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+        // a a a a a
+        
+        for(int i = 0; i<n; i++) {
+            dp[i][i] = true;
+            for(int j = 0; j<=i; j++) {
+                if(s.charAt(i) == s.charAt(j) && (i-j <= 2 || dp[i-1][j+1] == true)) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
     public int countSubstrings(String s) {
         int n = s.length();
         int count = 0;
-
+        // return solveDp(s);
         for(int i = 0; i<n; i++) {
             int l = i, r = i;
 
