c3d98cb46ff33030c1b2977030eb1388fe7f15a5
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2024-10-23 09:22:29 +0530

    Time: 8 ms (68.39%), Space: 44.8 MB (24.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -16,8 +16,19 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
         int sum = Arrays.stream(nums).sum();
-        int[][] dp = new int[n][(2*sum)+1];
-        for(int[] arr: dp) Arrays.fill(arr, Integer.MIN_VALUE);
-        return solve(nums, 0, 0, target, sum, dp);
+        int[][] dp = new int[n+1][(2*sum)+1];
+        dp[0][sum+nums[0]] = 1;
+        dp[0][sum-nums[0]] += 1;
+        // for(int[] arr: dp) Arrays.fill(arr, Integer.MIN_VALUE);
+        // return solve(nums, 0, 0, target, sum, dp);
+        for(int i = 1; i<n; i++)  {
+            for(int j = -sum; j<=sum; j++) {
+                if(dp[i-1][sum + j] > 0) {
+                    dp[i][j + nums[i] + sum] += dp[i-1][j+sum];
+                    dp[i][j - nums[i] + sum] += dp[i-1][j+sum];
+                }
+            }
+        }
+        return Math.abs(target) > sum ? 0 : dp[n-1][target+sum];
     }
 }
