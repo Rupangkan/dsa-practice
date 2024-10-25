75d7b8499f6f94515e012fc1b51bf9cded122763
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2024-10-25 22:25:42 +0530

    Time: 1 ms (55.84%), Space: 42.8 MB (34.02%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -9,10 +9,26 @@ class Solution {
         }
     }
 
+    void solvePowerSet(int[] nums, List<List<Integer>> res, List<Integer> curr) {
+        int n = nums.length;
+        int powerEnd = 1<<n;
+        
+        for(int i = 0; i<powerEnd; i++) {
+            List<Integer> currList = new ArrayList<>();
+            int currVal = i;
+            for(int j = 0; j<n; j++) {
+                if((currVal&1) == 1) currList.add(nums[j]);
+                currVal = currVal>>1;
+            }
+            res.add(new ArrayList<>(currList));
+        }
+    }
+
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        solve(0, nums, res, curr, nums.length);
+        // solve(0, nums, res, curr, nums.length);
+        solvePowerSet(nums, res, curr);
         return res;
     }
 }
