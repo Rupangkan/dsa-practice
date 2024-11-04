6dd2a0b1a7f8a14c4d0b9e50d48f29982cfb7e21
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2024-11-04 21:51:33 +0530

    Time: 6 ms (5.93%), Space: 41.7 MB (30.52%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -15,11 +15,30 @@ class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int n = days.length, m = costs.length, last = days[n-1];
         int[] dp = new int[last+1];
-        Arrays.fill(dp, -1);
+        // Arrays.fill(dp, -1);
         int[] daysGone = new int[] {1, 7, 30};
 
         Set<Integer> set = new HashSet<>();
         for(int i = 0; i<n; i++) set.add(days[i]);
-        return solveRecur(1, days, costs, set, dp);
+
+        int c = 0;
+
+        for(int i = 1; i<=last; i++) {
+            if(i < days[c]) {
+                dp[i] = dp[i-1];
+                continue;
+            }
+            c++;
+            int day30 = 30, day7 = 7, day1 = 1;
+            // if(i+day1 > last) day1 = last;
+            // if(i+day7 > last) day7 = last;
+            // if(i+day30 > last) day30 = last;
+            dp[i] = Math.min(dp[i - 1] + costs[0],
+                        Math.min(dp[Math.max(0, i - 7)] + costs[1],
+                                dp[Math.max(0, i - 30)] + costs[2]));
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[last];
+        // return solveRecur(1, days, costs, set, dp);
     }
 }
