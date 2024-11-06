59c14e9045b6b0ee146d8abf579e8e43894d9dcd
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2024-11-06 18:10:52 +0530

    Time: 1 ms (39.31%), Space: 43 MB (62.31%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,15 +1,20 @@
 class Solution {
-    // int solve(int idx, int[] cost, int[] dp, int n) {
-    //     if(idx >= n-1) return 0;
+    int solve(int idx, int[] cost, int[] dp, int n) {
+        if(idx >= n) return 0;
+        
+        if(dp[idx] != -1) return dp[idx];
 
-    //     int pickOne = cost[idx] + solve(idx + 1, cost, dp, n);
-    //     int pickTwo = cost[idx] + solve(idx + 2, cost, dp, n);
+        int pickOne = cost[idx] + solve(idx + 1, cost, dp, n);
+        int pickTwo = cost[idx] + solve(idx + 2, cost, dp, n);
 
+        return dp[idx] = Math.min(pickOne, pickTwo);
+    }
 
-    // }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return Math.min(solve(0, cost, dp, n), solve(1, cost, dp, n));
         dp[0] = cost[0];
         dp[1] = cost[1];
 
@@ -18,7 +23,7 @@ class Solution {
             int pickTwo = cost[i] + dp[i-2];
             dp[i] = Math.min(pickOne, pickTwo);
         }
-        System.out.println(Arrays.toString(dp));
+        // System.out.println(Arrays.toString(dp));
         return Math.min(dp[n-1], dp[n-2]);
     }
 }
