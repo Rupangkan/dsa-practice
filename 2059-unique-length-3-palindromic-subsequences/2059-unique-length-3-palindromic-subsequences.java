71e0933bd6ecadf7ce24820e13308793f830d9c3
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-01-04 10:25:36 +0530

    Time: 204 ms (51.05%), Space: 45.4 MB (71.2%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int[] first = new int[26];
+        int[] last = new int[26];
+        int n = s.length(), ans = 0;
+        Arrays.fill(first, -1);
+
+        for(int i = 0; i<n; i++) {
+            int curr = s.charAt(i) - 'a';
+            if(first[curr] == -1) first[curr] = i;
+            last[curr] = i;
+        }
+        for(int i = 0; i<26; i++) {
+            if(first[i] == -1) continue;
+            Set<Character> bet = new HashSet<>();
+            for(int j = first[i] + 1; j<last[i]; j++) bet.add(s.charAt(j));
+            ans += bet.size();
+        }
+        return ans;
+
+    }
+}
