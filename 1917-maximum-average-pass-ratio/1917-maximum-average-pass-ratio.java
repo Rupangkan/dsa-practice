b409c9448f75d5cb0f273cce986f8868cfa6c54e
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-09-01 17:03:34 +0530

    Time: 362 ms (50.26%), Space: 95.6 MB (60.62%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -1,43 +1,24 @@
 class Solution {
-    double getProfit(int p, int t) {
-        return ((double) (p + 1)/(t + 1) - (double) p / t);
-    }
 
+    private double gain(int p, int t) {
+        return (double)(p + 1) / (t + 1) - (double)p / t;
+    }
     public double maxAverageRatio(int[][] classes, int extraStudents) {
         PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
-        int n = classes.length;
-
-        for(int[] curr: classes) {
-            int p = curr[0];
-            int t = curr[1];
-            double pro = getProfit(p, t);
-            pq.offer(new double[] { pro, p, t });
+        for (int[] c : classes) {
+            pq.offer(new double[]{gain(c[0], c[1]), c[0], c[1]});
         }
-
-        while(extraStudents > 0) {
-            double[] curr = pq.poll();
-            double currP = curr[0];
-            int p = (int) curr[1];
-            int t = (int) curr[2];
-
-            pq.offer(
-                new double[] {
-                    getProfit(p + 1, t + 1),
-                    p + 1, t + 1
-                }
-            );
-
-            extraStudents--;
+        while (extraStudents-- > 0) {
+            double[] cur = pq.poll();
+            int p = (int) cur[1], t = (int) cur[2];
+            p++; t++;
+            pq.offer(new double[]{gain(p, t), p, t});
         }
-
         double ans = 0;
-        while(!pq.isEmpty()) {
-            double[] curr = pq.poll();
-            int p = (int) curr[1];
-            int t = (int) curr[2];
-            ans += (double) p / t;
+        while (!pq.isEmpty()) {
+            double[] cur = pq.poll();
+            ans += (double) cur[1] / cur[2];
         }
-
-        return ans/n;
+        return ans / classes.length;
     }
 }
