bd83bd21a1096fb203956b3ee93221e8c4e2e5f7
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-09-27 15:52:12 +0530

    Time: 9 ms (43.42%), Space: 46.5 MB (86.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,6 +1,6 @@
 class Solution {
-
-    public int[][] merge(int[][] arr) {
+    // O(nlogn) + O(2N)
+    private int[][] better(int[][] arr) {
         Arrays.sort(arr, (a, b) -> a[0] - b[0]);
         int n = arr.length;
         List<int[]> ans = new ArrayList<>();
@@ -17,5 +17,28 @@ class Solution {
             ans.add(new int[] {start, end});
         }
         return ans.toArray(new int[ans.size()][2]);
+    }
+
+    private int[][] optimal(int[][] arr) {
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        int n = arr.length;
+        List<int[]> ans = new ArrayList<>();
+
+        for(int i = 0; i<n; i++) {
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            if(ans.isEmpty() || ans.get(ans.size() - 1)[1] < start) {
+                ans.add(new int[] { start, end });
+            } else {
+                end = Math.max(end, ans.get(ans.size() - 1)[1]);
+                ans.get(ans.size()-1)[1] = end;
+            }
+        }
+        return ans.toArray(new int[ans.size()][2]);
+    }
+
+    public int[][] merge(int[][] arr) {
+       return optimal(arr);
     }
 }
