35db8bdaee5dfa7229ca2f371e4cba80bf1641cf
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-09-28 12:46:29 +0530

    Time: 1 ms (87.22%), Space: 42.3 MB (25.43%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,31 +1,23 @@
 class Solution {
-    private List<Integer> generateRow(List<List<Integer>> ans, int n, int numRows) {
-        List<Integer> arr = new ArrayList<>();
-        if(n == 0) {
-            arr.add(1);
-            return arr;
-        } 
-
-        for(int i = 0; i<=n; i++) {
-            if(i == 0 || i == n) {
-                arr.add(1);
-                continue;
-            }
-            int left = ans.get(n-1).get(i-1);
-            int right = ans.get(n-1).get(i);
-            arr.add(left + right);
-        }
-        return arr;
-    }
-
     private List<List<Integer>> optimal(int num) {
-        List<List<Integer>> arr = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        
         for(int i = 0; i<num; i++) {
-            List<Integer> row = new ArrayList<>();
-            row = generateRow(arr, i, num);
-            arr.add(row);
+            List<Integer> curr = new ArrayList<>();
+            if(i == 0) curr.add(1);
+            else {
+                for(int j = 0; j<=i; j++) {
+                    if(j == 0 || j == i) curr.add(1);
+                    else {
+                        int left = ans.get(i-1).get(j-1);
+                        int right = ans.get(i-1).get(j);
+                        curr.add(left + right);
+                    }
+                } 
+            }
+            ans.add(curr);
         }
-        return arr;
+        return ans;
     }
 
     public List<List<Integer>> generate(int numRows) {
