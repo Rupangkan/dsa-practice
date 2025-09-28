191d855d7988246305d9ca0007c181c4ff9b377a
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-09-28 14:39:29 +0530

    Time: 8 ms (84.92%), Space: 46.7 MB (40.47%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,44 +1,19 @@
 class Solution {
-    // O(nlogn) + O(2N)
-    private int[][] better(int[][] arr) {
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-        int n = arr.length;
+    public int[][] merge(int[][] nums) {
+        Arrays.sort(nums, (a, b) -> a[0] - b[0]);
         List<int[]> ans = new ArrayList<>();
-        for(int i = 0; i<n; i++) {
-            int start = arr[i][0];
-            int end = arr[i][1];
-            if(!ans.isEmpty() && end <= ans.get(ans.size()-1)[1]) continue;
-
-            for(int j = i+1; j<n; j++) {
-                if(arr[j][0] <= end) {
-                    end = Math.max(end, arr[j][1]);
-                } else break;
-            }
-            ans.add(new int[] {start, end});
-        }
-        return ans.toArray(new int[ans.size()][2]);
-    }
-
-    private int[][] optimal(int[][] arr) {
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-        int n = arr.length;
-        List<int[]> ans = new ArrayList<>();
+        int n = nums.length;
 
         for(int i = 0; i<n; i++) {
-            int start = arr[i][0];
-            int end = arr[i][1];
-
-            if(ans.isEmpty() || ans.get(ans.size() - 1)[1] < start) {
-                ans.add(new int[] { start, end });
+            int start = nums[i][0];
+            int end = nums[i][1];
+            if(ans.isEmpty() || ans.get(ans.size()-1)[1] < start) {
+                ans.add(new int[] {start, end});
             } else {
-                end = Math.max(end, ans.get(ans.size() - 1)[1]);
+                end = Math.max(end, ans.get(ans.size()-1)[1]);
                 ans.get(ans.size()-1)[1] = end;
             }
         }
         return ans.toArray(new int[ans.size()][2]);
-    }
-
-    public int[][] merge(int[][] arr) {
-       return optimal(arr);
     }
 }
