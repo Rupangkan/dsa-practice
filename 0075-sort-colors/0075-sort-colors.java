2803974f097c485d96de7d39aa3da8d8628397b2
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-09-28 14:17:31 +0530

    Time: 0 ms (100%), Space: 42.1 MB (48.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,55 +1,28 @@
 class Solution {
-    private void better(int[] nums) {
-         int zeroC = 0, oneC = 0, twoC = 0, n = nums.length;
-
-        for(int i = 0; i<n; i++) {
-            if(nums[i] == 0) zeroC++;
-            else if(nums[i] == 1) oneC++;
-            else twoC++;
-        }
-
-        int idx = 0;
-        while(zeroC > 0) {
-            nums[idx] = 0;
-            zeroC--;
-            idx++;
-        }
-        while(oneC > 0) {
-            nums[idx] = 1;
-            oneC--;
-            idx++;
-        }
-        while(twoC > 0) {
-            nums[idx] = 2;
-            twoC--;
-            idx++;
-        }
-    }
-        
-    private void swap(int[] nums, int one, int two) {
-        int t = nums[one];
-        nums[one] = nums[two];
-        nums[two] = t;   
-    }
-
-    private void optimal(int[] nums) {
-        int n = nums.length;
-        int high = nums.length - 1, mid = 0, low = 0;
-
-        while(mid <= high) {
-            if(nums[mid] == 0) {
-                swap(nums, mid, low);
-                low++;
-                mid++;
-            } else if(nums[mid] == 1) mid++;
-            else if (nums[mid] == 2) {
-                swap(nums, mid, high);
-                high--;
-            }
-        }
+    // nums[mid] == 1 --> mid++
+    // nums[mid] == 0 --> swap(low, mid), mid++, low++;
+    // nums[high] == 2 --> swap(high, mid), high--;
+    private void swap(int[] nums, int ind1, int ind2) {
+        int t = nums[ind1];
+        nums[ind1] = nums[ind2];
+        nums[ind2] = t;
     }
 
     public void sortColors(int[] nums) {
-        optimal(nums);
+        int n = nums.length;
+        int low = 0, mid = 0, high = n-1;
+
+        while(mid<=high) {
+            if(nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else if(nums[mid] == 2) {
+                swap(nums, high, mid);
+                high--;
+            }
+        }
     }
 }
