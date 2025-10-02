5382f1d0a6262c3e62c56fe6bc357e7b37cf0741
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-02 09:50:45 +0530

    Time: 1088 ms (6.93%), Space: 53.2 MB (51.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -12,7 +12,33 @@ class Solution {
         }
         return -1;
     }
-    
+
+    // O (nlogn) + O (N)
+    public int better(int[] nums) {
+        // use hashmap and store the values and count;
+        return -1;
+    }
+
+    // Moore's voting algorithm, LEARN THE INTUITION
+    private int optimal(int[] nums) {
+        int n = nums.length;
+        int curr = 0;
+        int count = 0;
+
+        for(int i = 0; i<n; i++) {
+            if(count == 0) {
+                curr = nums[i];
+                count = 1;
+            } else if(nums[i] == curr) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return curr;
+    }
+
     public int majorityElement(int[] nums) {
         return brute(nums);
     }
