e404917a1305447a4747a6c7008b998a91e48ee4
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-03 11:04:08 +0530

    Time: 0 ms (100%), Space: 40.3 MB (89.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,31 @@
 class Solution {
-    private int findPath(int i, int j, int m, int n, int [][] dp) {
+    private int better(int i, int j, int m, int n, int [][] dp) {
         // int count = 0;
         if(i >= n || j >= m) return 0;
         if(i == n - 1 && j == m - 1) return 1;
         if(dp[i][j] != Integer.MIN_VALUE) return dp[i][j];
 
-        int down = findPath(i, j + 1, m, n, dp);
-        int right = findPath(i + 1, j, m, n, dp);
+        int down = better(i, j + 1, m, n, dp);
+        int right = better(i + 1, j, m, n, dp);
         return dp[i][j] = down + right;
+    }
+
+    // Combination problem nCr or nCd
+    private int optimal(int m, int n) {
+        int N = n + m - 2;
+        int D = m - 1;
+        long ans = 1;
+
+        for(int i = 1; i<=D; i++) {
+            ans = ans * (N - D + i)/i;
+        }
+        return (int) ans;
     }
 
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[n][m];
         for(int[] a: dp) Arrays.fill(a, Integer.MIN_VALUE);
-        return findPath(0, 0, m, n, dp);
+        // return better(0, 0, m, n, dp);
+        return optimal(m, n);
     }
 }
