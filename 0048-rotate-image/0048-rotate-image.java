1ca85f2f7445f24d4847b3da36bb65b9682980d0
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-12 15:12:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (78.9%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,29 +1,29 @@
 class Solution {
 
-    private void swap(int[][] nums, int ind1, int ind2) {
-        int t = nums[ind1][ind2];
-        nums[ind1][ind2] = nums[ind2][ind1];
-        nums[ind2][ind1] = t;
+    private void swap(int[][] mat, int row, int start, int end) {
+        int temp = mat[row][start];
+        mat[row][start] = mat[row][end];
+        mat[row][end] = temp;
     }
 
-    private void swapRow(int[][] nums, int ind1, int ind2, int rowNum) {
-        int t = nums[rowNum][ind2];
-        nums[rowNum][ind2] = nums[rowNum][ind1];
-        nums[rowNum][ind1] = t;
-    }
-
-    private void reverse(int[][] mat, int ind) {
-        int start = 0, end = mat.length - 1;
-
-        while(start<end) {
-            swapRow(mat, start, end, ind);
+    private void reverse(int[][] mat, int row) {
+        int start = 0, end = mat[0].length-1;
+        while(start < end) {
+            swap(mat, row, start, end);
             start++;
             end--;
         }
     }
 
+    private void swap(int[][] mat, int ind1, int ind2) {
+        int temp = mat[ind1][ind2];
+        mat[ind1][ind2] = mat[ind2][ind1];
+        mat[ind2][ind1] = temp;
+    }
+
     public void rotate(int[][] mat) {
         int n = mat.length;
+        int m = mat[0].length;
 
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<i; j++) {
