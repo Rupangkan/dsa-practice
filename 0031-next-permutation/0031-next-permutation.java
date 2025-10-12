ea11c723ea465d542ac9335c3cfa2e423f929dd7
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-12 11:44:58 +0530

    Time: 0 ms (100%), Space: 43.1 MB (57.86%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,15 +1,16 @@
 class Solution {
+
     private void swap(int[] nums, int ind1, int ind2) {
-        int t = nums[ind1];
+        int temp = nums[ind1];
         nums[ind1] = nums[ind2];
-        nums[ind2] = t;
+        nums[ind2] = temp;
     }
 
     private void reverse(int[] nums, int start, int end) {
         while(start<end) {
             swap(nums, start, end);
-            end--;
             start++;
+            end--;
         }
     }
 
@@ -18,21 +19,22 @@ class Solution {
         int idx = -1;
 
         for(int i = n-1; i>0; i--) {
-            if(nums[i] > nums[i-1]) {
+            if(nums[i-1] < nums[i]) {
                 idx = i-1;
                 break;
             }
         }
-
-        if(idx == -1) reverse(nums, 0, n-1);
-        else {
+        // 1 3 2 5 4 3 2
+        if(idx == -1) {
+            reverse(nums, 0, n-1);
+        } else {
             for(int i = n-1; i>idx; i--) {
                 if(nums[i] > nums[idx]) {
                     swap(nums, i, idx);
                     break;
                 }
             }
-            reverse(nums, idx+1, n-1);
+            reverse(nums, idx + 1, n-1);
         }
     }
 }
