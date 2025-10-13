1e9c4fa2bae0560671944c831ddbe1fb18afd41e
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-13 21:59:16 +0530

    Time: 0 ms (100%), Space: 40.3 MB (89.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,31 +1,15 @@
 class Solution {
-    private int better(int i, int j, int m, int n, int [][] dp) {
-        // int count = 0;
-        if(i >= n || j >= m) return 0;
-        if(i == n - 1 && j == m - 1) return 1;
-        if(dp[i][j] != Integer.MIN_VALUE) return dp[i][j];
+    private int paths(int m, int n, int[][] dp) {
+        if(m == 0 && n == 0) return 1;
+        if(m < 0 || n < 0) return 0;
+        if(dp[m][n] != Integer.MIN_VALUE) return dp[m][n];
 
-        int down = better(i, j + 1, m, n, dp);
-        int right = better(i + 1, j, m, n, dp);
-        return dp[i][j] = down + right;
-    }
-
-    // Combination problem nCr or nCd
-    private int optimal(int m, int n) {
-        int N = n + m - 2;
-        int D = m - 1;
-        long ans = 1;
-
-        for(int i = 1; i<=D; i++) {
-            ans = ans * (N - D + i)/i;
-        }
-        return (int) ans;
+        return dp[m][n] = paths(m-1, n, dp) + paths(m, n-1, dp); 
     }
 
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[n][m];
-        for(int[] a: dp) Arrays.fill(a, Integer.MIN_VALUE);
-        // return better(0, 0, m, n, dp);
-        return optimal(m, n);
+        int[][] dp = new int[m][n];
+        for(int[] arr: dp) Arrays.fill(arr, Integer.MIN_VALUE);
+        return paths(m-1, n-1, dp);
     }
 }
