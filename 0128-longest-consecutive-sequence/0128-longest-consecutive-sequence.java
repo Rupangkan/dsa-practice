8dadee0f69d877e7f90bb55e216866643f80df17
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-14 00:34:15 +0530

    Time: 31 ms (58.39%), Space: 67.8 MB (29.18%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,56 +1,23 @@
 class Solution {
-    // On2
-    private int brute(int[] nums) {return -1;}
-
-
-    // Onlogn + ON
-    private int better(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-
-        if(n == 0) return 0;
-
-        int lastSmaller = Integer.MIN_VALUE;
-        int longest = 1, currCount = 0;
-
-        for(int i = 0; i<n; i++) {
-            if(nums[i] - 1 == lastSmaller) {
-                currCount++;
-                lastSmaller = nums[i];
-            } else if(nums[i] != lastSmaller) {
-                currCount = 1;
-                lastSmaller = nums[i];
-            }
-            longest = Math.max(longest, currCount);
-        }
-        return longest;
-    }
-
-    private int optimal(int[] nums) {
+    public int longestConsecutive(int[] nums) {
         int n = nums.length;
         Set<Integer> set = new HashSet<>();
-        int currCount = 0, longest = 0;
+        int max = 0;
+
         for(int i = 0; i<n; i++) {
             set.add(nums[i]);
         }
 
-        for(int i: set) {
-            if(!set.contains(i-1)) {
-                int curr = i;
-                currCount = 1;
-
+        for(int curr: set) {
+            int count = 1;
+            if(!set.contains(curr - 1)) {
                 while(set.contains(curr + 1)) {
-                    curr++;
-                    currCount++;
+                    count++;
+                    curr = curr + 1;
                 }
             }
-            longest = Math.max(longest, currCount);
+            max = Math.max(count, max);
         }
-        return longest;
-    }
-
-    public int longestConsecutive(int[] nums) {
-        // return better(nums);
-        return optimal(nums);
+        return max;
     }
 }
