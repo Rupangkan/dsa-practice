2f1de6e5ac9dd62cdf94e1d948395d4ef3204be4
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-13 21:44:31 +0530

    Time: 2 ms (74.47%), Space: 53.4 MB (31.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,45 +1,29 @@
 class Solution {
-    private int brute(int[] nums) {
+    public int majorityElement(int[] nums) {
         int n = nums.length;
-
-        for(int i = 0; i<n; i++) {
-            int currCount = 0;
-            int curr = nums[i];
-            for(int j = i; j<n; j++) {
-                if(curr == nums[j]) currCount++;
-            }
-            if(currCount > n/2) return curr;
-        }
-        return -1;
-    }
-
-    // O (nlogn) + O (N)
-    public int better(int[] nums) {
-        // use hashmap and store the values and count;
-        return -1;
-    }
-
-    // Moore's voting algorithm, LEARN THE INTUITION
-    private int optimal(int[] nums) {
-        int n = nums.length;
-        int curr = 0;
+        int ele1 = 0;
         int count = 0;
 
         for(int i = 0; i<n; i++) {
             if(count == 0) {
-                curr = nums[i];
-                count = 1;
-            } else if(nums[i] == curr) {
+                ele1 = nums[i];
                 count++;
             } else {
-                count--;
+                if(ele1 != nums[i]) {
+                    count--;
+                } else {
+                    count++;
+                }
             }
         }
 
-        return curr;
-    }
+        int c1 = 0;
 
-    public int majorityElement(int[] nums) {
-        return brute(nums);
+        for(int i = 0; i<n; i++) {
+            if(nums[i] == ele1) c1++;
+        }
+
+        if(c1 > n/2) return ele1;
+        return -1;
     }
 }
