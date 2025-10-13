967d88e5dff84c25567a04b147199098a1b409e1
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-13 21:52:16 +0530

    Time: 2 ms (99.74%), Space: 50.2 MB (77.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,34 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        int count1 = 0, count2 = 0;
-        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        int ele1 = 0, ele2 = 0, c1 = 0, c2 = 0;
 
         for(int i = 0; i<n; i++) {
-            if(count1 == 0 && ele2 != nums[i]) {
+            if(c1 == 0 && nums[i] != ele2) {
                 ele1 = nums[i];
-                count1 = 1;
-            } else if(count2 == 0 && nums[i] != ele1) {
+                c1++;
+            } else if(c2 == 0 && nums[i] != ele1) {
                 ele2 = nums[i];
-                count2 = 1;
-            } else if(nums[i] == ele1) count1++;
-            else if(nums[i] == ele2) count2++;
-            else {
-                count1--;
-                count2--;
+                c2++;
+            } else if(ele1 == nums[i]) c1++;
+            else if(ele2 == nums[i]) c2++;
+            else if(ele1 != nums[i] && ele2 != nums[i]) {
+                c1--;
+                c2--;
             }
         }
 
-        List<Integer> arr = new ArrayList<>();
-        int cnt1 = 0, cnt2 = 0;
+        int count1 = 0, count2 = 0;
 
         for(int i = 0; i<n; i++) {
-            if(nums[i] == ele1) cnt1++;
-            if(nums[i] == ele2) cnt2++;
+            if(nums[i] == ele1) count1++;
+            else if(nums[i] == ele2) count2++;
         }
-
-        if(cnt1 > n/3) arr.add(ele1);
-        if(cnt2 > n/3) arr.add(ele2);
-        return arr;
+        List<Integer> ans = new ArrayList<>();
+        if(count1 > n/3) ans.add(ele1);
+        if(count2 > n/3) ans.add(ele2);
+        return ans;
     }
 }
