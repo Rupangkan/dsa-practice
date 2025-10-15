227900ae9e7bd9dadaa51bf507106893770455a7
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-15 19:59:37 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31.35%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,7 +14,7 @@ class Node {
 */
 
 class Solution {
-    public Node copyRandomList(Node head) {
+    public Node better(Node head) {
         Node d1 = new Node(-1);
         Node temp = d1;
         Node tempHead = head;
@@ -36,6 +36,43 @@ class Solution {
         }
 
         return d1.next;
-        
+    }
+
+    private Node optimal(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            Node currNode = new Node(temp.val);
+            currNode.next = temp.next;
+            temp.next = currNode;
+            temp = temp.next.next;
+        }
+
+        temp = head;
+
+        while(temp != null) {
+            Node currNode = temp.next;
+            if(temp.random != null)
+                currNode.random = temp.random.next;
+            else 
+                currNode.random = null;
+            temp = temp.next.next;
+        }
+
+        temp = head;
+        Node d1 = new Node(-1);
+        Node res = d1;
+
+        while(temp != null) {
+            res.next = temp.next;
+            res = res.next;
+            temp.next = temp.next.next;
+            temp = temp.next;
+        }
+
+        return d1.next;
+    }
+
+    public Node copyRandomList(Node head) {
+        return optimal(head);
     }
 }
