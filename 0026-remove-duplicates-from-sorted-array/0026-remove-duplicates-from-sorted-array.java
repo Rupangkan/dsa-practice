0dd896046eee6365d068dc07609bca4a5ebc56fd
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-17 09:19:50 +0530

    Time: 0 ms (100%), Space: 44.9 MB (62.91%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -14,7 +14,23 @@ class Solution {
 
         return set.size();
     }
+
+    private int optimal(int[] nums) {
+        int n = nums.length;
+
+        int idx = 0;
+
+        for(int i = 1; i<n; i++) {
+            if(nums[i] != nums[idx]) {
+                idx++;
+                nums[idx] = nums[i];
+            }
+        }
+        return idx + 1;
+    }
+
     public int removeDuplicates(int[] nums) {
-        return brute(nums);
+        // return brute(nums);
+        return optimal(nums);
     }
 }
