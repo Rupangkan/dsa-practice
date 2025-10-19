878a899923d2c91540da8ae53828b731ff99897c
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-19 12:34:57 +0530

    Time: 0 ms (100%), Space: 45 MB (6.67%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,65 +14,39 @@ class Node {
 */
 
 class Solution {
-    public Node better(Node head) {
-        Node d1 = new Node(-1);
-        Node temp = d1;
-        Node tempHead = head;
-        HashMap<Node, Node> map = new HashMap<>();
-
-        while(tempHead != null) {
-            temp.next = new Node(tempHead.val);
-            map.put(tempHead, temp.next);
-            temp = temp.next;
-            tempHead = tempHead.next;
-        }
-
-        temp = d1.next;
-        tempHead = head;
-        while(tempHead != null) {
-            temp.random = map.get(tempHead.random);
-            tempHead = tempHead.next;
-            temp = temp.next;
-        }
-
-        return d1.next;
-    }
-
-    private Node optimal(Node head) {
+    public Node copyRandomList(Node head) {
         Node temp = head;
+
         while(temp != null) {
-            Node currNode = new Node(temp.val);
-            currNode.next = temp.next;
-            temp.next = currNode;
+            Node curr = new Node(temp.val);
+            curr.next = temp.next;
+            temp.next = curr;
             temp = temp.next.next;
         }
 
         temp = head;
 
-        while(temp != null) {
-            Node currNode = temp.next;
-            if(temp.random != null)
-                currNode.random = temp.random.next;
-            else 
-                currNode.random = null;
+        while(temp != null && temp.next != null) {
+            Node curr = temp.next;
+            if(temp.random != null) {
+                curr.random = temp.random.next;
+            } else {
+                curr.random = null;
+            }
             temp = temp.next.next;
         }
 
         temp = head;
         Node d1 = new Node(-1);
-        Node res = d1;
+        Node d2 = d1;
 
-        while(temp != null) {
-            res.next = temp.next;
-            res = res.next;
+        while(temp != null && temp.next != null) {
+            d2.next = temp.next;
+            d2 = d2.next;
             temp.next = temp.next.next;
             temp = temp.next;
-        }
+        }   
 
         return d1.next;
-    }
-
-    public Node copyRandomList(Node head) {
-        return optimal(head);
     }
 }
