489ae8ec0921cfbcb0c26a21c7c9a1ecb127dd66
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-21 11:17:31 +0530

    Time: 3 ms (69.88%), Space: 43.2 MB (73.13%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    private void test(int[] can, int t, int idx, List<List<Integer>> ans, List<Integer> temp) {
+        // if(idx == can.length) return;
+        if(t == 0) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+  
+        for(int i = idx; i<can.length; i++) {
+            if(i > idx && can[i] == can[i-1]) continue;
+            // if(can[i] > t) break; 
+            if(t - can[i] >= 0) {
+                temp.add(can[i]);
+                test(can, t - can[i], i + 1, ans, temp);
+                temp.remove(temp.size()-1);
+            }
+    
+        }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] can, int t) {
+        Arrays.sort(can);
+        List<List<Integer>> ans = new ArrayList<>();
+        test(can, t, 0, ans, new ArrayList<>());
+        return ans;
+    }
+}
