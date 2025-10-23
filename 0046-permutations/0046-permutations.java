1edb55436eeab65cbd43a0980d33c39ebe1e1974
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-23 09:24:18 +0530

    Time: 1 ms (93%), Space: 45 MB (10.57%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,5 +1,7 @@
 class Solution {
-
+    // Better
+    // O n! * n 
+    // O n * n
     private void test(int n, int[] nums, List<List<Integer>> ans, List<Integer> temp, boolean[] freq) {
         if(temp.size() == n) {
             ans.add(new ArrayList<>(temp));
@@ -16,11 +18,36 @@ class Solution {
             }
         }
     }
+
+    private void optimal(int idx, int n, int[] nums, List<List<Integer>> ans) {
+        if(idx == n) {
+            List<Integer> t = new ArrayList<>();
+            for(int i = 0; i<n; i++) {
+                t.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(t));
+            return;
+        }
+
+        for(int i = idx; i<n; i++) {
+            swap(i, idx, nums);
+            optimal(idx+1, n, nums, ans);
+            swap(i, idx, nums);
+        }
+    }
+
+    private void swap(int ind1, int ind2, int[] nums) {
+        int temp = nums[ind1];
+        nums[ind1] = nums[ind2];
+        nums[ind2] = temp;
+    }
+
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         boolean[] freq = new boolean[n+1];
-        test(n, nums, ans, new ArrayList<>(), freq);
+        // test(n, nums, ans, new ArrayList<>(), freq);
+        optimal(0, n, nums, ans);
         return ans;
     }
 }
