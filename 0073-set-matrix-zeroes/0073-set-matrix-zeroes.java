dd8601192cdaf8ade1d68165a190a2dbeefe8a58
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-10-26 11:55:48 +0530

    Time: 1 ms (68.21%), Space: 46.2 MB (8.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,56 +1,27 @@
 class Solution {
-    private void better(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[] rows = new int[n];
-        int[] cols = new int[m];
-
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<m; j++) {
-                if(mat[i][j] == 0) {
-                    rows[i] = 1;
-                    cols[j] = 1;
-                }
-            }
-        }
-
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<m; j++) {
-                if(rows[i] == 1 || cols[j] == 1) {
-                    mat[i][j] = 0;
-                }
-            }
-        }
-    }
-
     public void setZeroes(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
+
         int col0 = 1;
 
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<m; j++) {
                 if(mat[i][j] == 0) {
                     mat[i][0] = 0;
-                    if(j != 0) {
-                        mat[0][j] = 0;
-                    } else {
+                    if(j == 0) {
                         col0 = 0;
+                    } else {
+                        mat[0][j] = 0;
                     }
-                    // if(i == 0 && j == 0) {
-                    //     col0 = 0;
-                    // } else {
-                    //     mat[i][0] = 0;
-                    //     mat[0][j] = 0;
-                    // }
-                }
+                } 
             }
         }
 
         for(int i = 1; i<n; i++) {
             for(int j = 1; j<m; j++) {
-                if(mat[i][0] == 0 || mat[0][j] == 0) {
-                    mat[i][j] = 0;
+                if(mat[i][j] != 0) {
+                    if(mat[i][0] == 0 || mat[0][j] == 0) mat[i][j] = 0;
                 }
             }
         }
