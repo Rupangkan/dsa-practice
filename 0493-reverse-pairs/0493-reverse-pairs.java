e8d3de64f8d2a9625447afa7e3a21eac7039ac28
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-11-02 15:21:43 +0530

    Time: 80 ms (18.69%), Space: 59 MB (5.26%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,55 +1,56 @@
 class Solution {
-    private int countReverse(int[] nums, int low, int mid, int high) {
+    private int countReverse(int low, int mid, int high, int[] arr) {
         int count = 0;
         int right = mid + 1;
-
         for(int i = low; i<=mid; i++) {
-            while(right <= high && (long)nums[i] > (long)2*nums[right]) right++;
-            count += (right - (mid + 1));
+            while(right <= high && (long)arr[i]>(long)2*arr[right]) right++;
+            count = count + (right - (mid + 1));
         }
         return count;
     }
 
-    private void merge(int[] nums, int low, int mid, int high) {
-        List<Integer> temp = new ArrayList<>();
+    private void merge(int[] nums, int low, int high, int mid) {
         int left = low;
         int right = mid + 1;
 
+        ArrayList<Integer> arr = new ArrayList<>();
+
         while(left <= mid && right <= high) {
             if(nums[left] > nums[right]) {
-                temp.add(nums[right++]);
+                arr.add(nums[right]);
+                right++;
             } else {
-                temp.add(nums[left++]);
+                arr.add(nums[left]);
+                left++;
             }
         }
 
         while(left <= mid) {
-            temp.add(nums[left++]);
+            arr.add(nums[left++]);
         }
 
         while(right <= high) {
-            temp.add(nums[right++]);
+            arr.add(nums[right++]);
         }
 
         for(int i = low; i<=high; i++) {
-            nums[i] = temp.get(i-low);
+            nums[i] = arr.get(i-low);
         }
     }
 
-    private int sol(int[] nums, int low, int high) {
+    private int optimal(int low, int high, int[] nums) {
         int count = 0;
-        if(low >= high) return (int)count;
+        if(low >= high) return count;
         int mid = (high + low)/2;
-        count += sol(nums, low, mid);
-        count += sol(nums, mid + 1, high);
-        count += countReverse(nums, low, mid, high);
-        merge(nums, low, mid, high);
-        return (int)count;
+        count += optimal(low, mid, nums);
+        count += optimal(mid + 1, high, nums);
+        count += countReverse(low, mid, high, nums);
+        merge(nums, low, high, mid);
+        return count;
     }
 
     public int reversePairs(int[] nums) {
         int n = nums.length;
-        int low = 0, high = n-1;
-        return sol(nums, low, high);
+        return optimal(0, n-1, nums);
     }
 }
