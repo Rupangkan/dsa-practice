357942cc86f036726062808c74065e2acd1e3930
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-11-09 13:12:58 +0530

    Time: 1 ms (58.37%), Space: 46.7 MB (6.4%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,4 +1,6 @@
 class Solution {
+    // ON
+    // ON
     private int brute(int[] nums) {
         int n = nums.length;
         Set<Integer> set = new HashSet<>(); 
@@ -15,17 +17,20 @@ class Solution {
         return set.size();
     }
 
+    // ON
+    // O1
     private int optimal(int[] nums) {
         int n = nums.length;
-
         int idx = 0;
 
+        // 1, 1, 2, 2, 2, 3, 4,
         for(int i = 1; i<n; i++) {
             if(nums[i] != nums[idx]) {
                 idx++;
                 nums[idx] = nums[i];
             }
         }
+
         return idx + 1;
     }
 
