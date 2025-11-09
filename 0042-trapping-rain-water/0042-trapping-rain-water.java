286c1c7100ce3b326ceb0efe994bf39f90e49c95
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-11-09 13:07:47 +0530

    Time: 0 ms (100%), Space: 47.7 MB (6.47%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,42 +1,14 @@
 class Solution {
-    // O3N
-    // O2N or use ON just take the back and for front just keep a max value while iterating
-    private int better(int[] h) {
+    public int trap(int[] h) {
         int n = h.length;
-        int[] back = new int[n];
-        int[] front = new int[n];
-
-        front[0] = h[0];
-        back[n-1] = h[n-1];
-
-        for(int i = 1; i<n; i++) {
-            front[i] = Math.max(h[i], front[i-1]);
-        }
-
-        for(int i = n-2; i>=0; i--) {
-            back[i] = Math.max(h[i], back[i+1]);
-        }
-        
-        int count = 0;
-        for(int i = 0; i<n; i++) {
-            if(h[i] < back[i] && h[i] < front[i]) {
-                int min = Math.min(back[i], front[i]);
-                count += min - h[i];
-            }
-        }
-
-        return count;
-    }
-
-    private int optimal(int[] h) {
-        int n = h.length;
-        int low = 0, high = n-1, leftMax = 0, rightMax = 0;
+        int low = 0, high = n - 1;
+        int leftMax = 0, rightMax = 0;
         int count = 0;
 
         while(low < high) {
             if(h[low] <= h[high]) {
                 if(leftMax > h[low]) {
-                    count += leftMax - h[low]; 
+                    count += leftMax - h[low];
                 } else {
                     leftMax = h[low];
                 }
@@ -50,11 +22,7 @@ class Solution {
                 high--;
             }
         }
-        return count;
-    }
 
-    public int trap(int[] height) {
-        // return better(height);
-        return optimal(height);
+        return count;
     }
 }
