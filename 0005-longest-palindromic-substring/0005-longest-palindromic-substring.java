db620dd4ee7821d426bd6c1ec8b7c3eed4e45f7d
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-12-04 08:45:14 +0530

    Time: 9 ms (96.27%), Space: 44.6 MB (46.76%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,5 +1,8 @@
 class Solution {
-    private String optimal(String s) {
+    // Optimal 
+    // On2
+    // O1
+    private String better(String s) {
         String ans = "";
         int len = 0, n = s.length();
 
@@ -31,7 +34,53 @@ class Solution {
         return ans;
     }
 
+    // Manacher's algorithm
+    // ON
+    // ON
+    private String optimal(String s) {
+        StringBuilder sPrime = new StringBuilder("#");
+        for(char c: s.toCharArray()) {
+            sPrime.append(c).append("#");
+        }
+
+        int n = sPrime.length();
+        int[] palinRadii = new int[n];
+        int center = 0, radius = 0;
+
+        for(int i = 0; i<n; i++) {
+            int mirr = 2 * center - i;
+
+            if(i < radius) {
+                palinRadii[i] = Math.min(radius - i, palinRadii[mirr]);
+            }
+
+            while((i + 1 + palinRadii[i] < n) && (i - 1 - palinRadii[i] >= 0) && 
+            sPrime.charAt(i+1+palinRadii[i]) == sPrime.charAt(i - 1 - palinRadii[i])) {
+                palinRadii[i]++;
+            }
+
+            if(i + palinRadii[i] > radius) {
+                center = i;
+                radius = i + palinRadii[i];
+            }
+        }
+
+        int maxLen = 0, centerIdx = 0;
+
+        for(int i = 0; i<n; i++) {
+            if(palinRadii[i] > maxLen) {
+                maxLen = palinRadii[i];
+                centerIdx = i;
+            }
+        }
+        
+        int startIdx = (centerIdx - maxLen) / 2;
+        String longest = s.substring(startIdx, startIdx + maxLen);
+        return longest;
+    }
+
     public String longestPalindrome(String s) {
+        // return better(s);
         return optimal(s);
     }
 }
