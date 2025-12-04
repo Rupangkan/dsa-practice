3135834c5a2eef82cca1375dd71e9a1e4b70c33a
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2025-12-04 08:20:39 +0530

    Time: 20 ms (41.54%), Space: 46.5 MB (25.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,54 +1,37 @@
 class Solution {
-
-    public String solveDp(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int len = 0;
+    private String optimal(String s) {
         String ans = "";
+        int len = 0, n = s.length();
+
         for(int i = 0; i<n; i++) {
-            dp[i][i] = true;
-            for(int j = 0; j<=i; j++) {
-                if(s.charAt(i)==s.charAt(j) && (i-j<=2 || dp[i-1][j+1] == true)) {
-                    dp[i][j] = true;
-                    if(i-j+1 > len) {
-                        len = i-j+1;
-                        ans = s.substring(j, i+1);
-                    }
+            int left = i, right = i;
+            // odd length
+            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                if(right - left + 1 > len) {
+                    len = right - left + 1;
+                    ans = s.substring(left, right+1);
                 }
+                left--;
+                right++;
+            }
+
+            left = i;
+            right = i+1;
+
+            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                if(right - left + 1 > len) {
+                    len = right - left + 1;
+                    ans = s.substring(left, right+1);
+                }
+                left--;
+                right++;
             }
         }
+
         return ans;
     }
 
     public String longestPalindrome(String s) {
-        int n = s.length();
-        int len = 0;
-        String ans = "";
-        if(n == 1) return s;
-        // return solveDp(s);
-        for(int i = 0; i<n; i++) {
-            int l = i, r = i;
-
-            while(l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
-                if(r - l + 1 > len) {
-                    ans = s.substring(l, r+1);
-                    len = r - l + 1;
-                }
-                l--;
-                r++;
-            }
-
-            l = i;
-            r = i+1;
-            while(l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
-                if(r - l + 1 > len) {
-                    ans = s.substring(l, r+1);
-                    len = r - l + 1;
-                }
-                l--;
-                r++;
-            }
-        }
-        return ans;
-    }  
+        return optimal(s);
+    }
 }
