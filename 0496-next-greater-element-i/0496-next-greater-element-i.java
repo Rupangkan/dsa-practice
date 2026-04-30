35a0338d58b3f1a6578fdc4ac0c625465589ae53
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2026-04-30 12:26:04 +0530

    Time: 2 ms (99.21%), Space: 45.7 MB (10.8%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,21 +1,25 @@
 class Solution {
+    // Optimal 
+    // O (n + m)
+    // O (n + 1001)
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         // 4, 1, 2, 3, 5, 8, 2, 1, 3, 4, 1
         Stack<Integer> st = new Stack<>();
-        int[] nextG = new int[10001];
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] nge = new int[10001];
 
-        for(int i = nums2.length - 1; i>=0; i--) {
+        for(int i = m-1; i>=0; i--) {
             while(!st.isEmpty() && st.peek() <= nums2[i]) st.pop();
-            nextG[nums2[i]] = st.isEmpty() ? -1 : st.peek();
+            if(st.isEmpty()) nge[nums2[i]] = -1;
+            else nge[nums2[i]] = st.peek();
             st.push(nums2[i]);
         }
 
-        int[] res = new int[nums1.length];
-
-        for(int i = 0; i<nums1.length; i++) {
-            res[i] = nextG[nums1[i]];
+        int[] res = new int[n];
+        for(int i = 0; i<n; i++) {
+            res[i] = nge[nums1[i]];
         }
-
         return res;
     }
 }
