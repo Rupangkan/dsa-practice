5a79db6b042cc36668f4319ddb1b11b6935586fe
Rupangkan Kalita <Every1isnotrupangkan@gmail.com>
2026-04-30 19:32:55 +0530

    Time: 1 ms (99.97%), Space: 77.3 MB (48.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = nums[0];
+        int max = nums[0], n = nums.length;
         int maxLocal = nums[0];
-        int n = nums.length;
-
+        
         for(int i = 1; i<n; i++) {
             maxLocal = Math.max(nums[i], nums[i] + maxLocal);
-            max = Math.max(max, maxLocal);
+            max = Math.max(maxLocal, max);
         }
-
         return max;
     }
 }
